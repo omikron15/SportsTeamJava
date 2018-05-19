7a56233a548e99f78f56e1459e3619c147a0a560
omikron15 <omikron15@hotmail.co.uk>
2018-05-19 14:44:55 +0100

    Adds manager class with constructor, getter and setters

---
+++ src/main/java/models/Manager.java
@@ -0,0 +1,50 @@
+package models;
+
+public class Manager {
+
+    private int id;
+    private String name;
+    private int age;
+    private Team team;
+
+    public Manager() {
+    }
+
+    public Manager(String name, int age, Team team) {
+        this.name = name;
+        this.age = age;
+        this.team = team;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
