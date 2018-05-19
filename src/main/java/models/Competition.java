2a5ab12e1cc75add748873ea0d8e56ead3f62b38
omikron15 <omikron15@hotmail.co.uk>
2018-05-19 14:49:25 +0100

    Adds Competition class with consturctors, getters and setters

---
+++ src/main/java/models/Competition.java
@@ -0,0 +1,42 @@
+package models;
+
+import java.util.List;
+
+public class Competition {
+
+    private int id;
+    private String name;
+    private List<Team> teams;
+
+    public Competition() {
+    }
+
+    public Competition(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
+}
