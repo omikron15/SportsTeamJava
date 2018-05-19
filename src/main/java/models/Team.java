7ec5b48d00f2aaf2fc6b4d7de54005f9aa481f6e
omikron15 <omikron15@hotmail.co.uk>
2018-05-19 14:39:16 +0100

    adds Team class with constructores, getting and setters

---
+++ src/main/java/models/Team.java
@@ -0,0 +1,62 @@
+package models;
+
+import java.util.List;
+
+public class Team {
+
+    private int id;
+    private String name;
+    private List<Player> players;
+    private Manager manager;
+    private List<Competition> competitions;
+
+    public Team() {
+    }
+
+    public Team(String name, Manager manager) {
+        this.name = name;
+        this.manager = manager;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+
+    public List<Competition> getCompetitions() {
+        return competitions;
+    }
+
+    public void setCompetitions(List<Competition> competitions) {
+        this.competitions = competitions;
+    }
+
+
+}
