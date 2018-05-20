0ffcbab52d6914b5bbcc160af5b66b07fd07c5ca
omikron15 <omikron15@hotmail.co.uk>
2018-05-20 13:49:26 +0100

    adds delete method to DBHelper class

--- src/main/java/db/DBHelper.java
+++ src/main/java/db/DBHelper.java
@@ -57,4 +57,18 @@ public class DBHelper {
         }
         return result;
     }
+
+    public static void delete(Object object){
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.delete(object);
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
 }
